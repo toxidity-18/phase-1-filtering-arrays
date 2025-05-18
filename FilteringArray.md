79b4d76a89e360a7d0bce1f232e0adae17f35919
toxidity-18 <desavigatunganga@gmail.com>
2025-05-18 04:32:09 +0300

    working and learning something new

---
+++ FilteringArray.md
@@ -0,0 +1,11 @@
+---
+14|May|25
+---
+
+# Introduction
+- How to find or allocate specific elements which meet a certain condition .
+- Method called filtering method : using filtering function .
+
+## Review 
+- We print out the firstName using array iteration . Code ln 41 to 46
+- Print out user firstName whose favoriteColor is Blue .
--- filteringArray.js
+++ filteringArray.js
@@ -1,3 +1,4 @@
+// array of flatbook users and with objects .
 const users = [
   {
     firstName: "Niky",
@@ -37,49 +38,27 @@ const users = [
   },
 ];
 
-// iterating to print out the firstName of each user 
-function printFirstName(collection){
+// Iterate and print out everyone first name .
+function firstNamePrint(collection){
   for (const user of collection){
     console.log(user.firstName)
   }
 }
-printFirstName(users)
-// iterating to print out the lastName of each user
-function printLastName(collection){
+
+// print out only users whose favorite color is blue 
+function blueFilter(collection){
   for (const user of collection){
-    console.log(user.lastName)
-  }
-}
-printLastName(users)
-
-// print out fav color blue 
-function blueColor(collection){
-  for (const user of collection){
-    if (user.favoriteColor === "Blue"){
-      console.log(user.firstName)
-    }       
-  }
-}
-blueColor(users)
-
-// filter collection of user whose favorite color is Red
-function colorFilter(collection,color){
-  for ( const user of sollection){
-    if ( user.favoriteColor === color){
-      console.log(user.firstName)
-    }     
-  }
-}
-colorFilter(users,"Red")
-
-
-// filter out favoriteAnimal and color : 'jaguar' and 'red'
-function filter(collection,attribute,value){
-  for(const user of collection){
-    if(user[attribute]===value){
+    if(user.favoriteColor==='Blue'){
       console.log(user.firstName)
     }
   }
 }
-filter(users,'favoriteAnimal','Jaguar ')
 
+// filter out  user whose favorite color is red 
+function redFilter(collection){
+  for(const user of collection){
+    if(user.favoriteColor==='Red'){
+      console.log(user.firstName)
+    }
+  }
+}
